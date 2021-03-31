5ce0a5ff3aa62273494300ca90baeee12b8bbb03
Adam_Codes <76964302+Adam0290@users.noreply.github.com>
2021-03-31 01:03:48 +0100

    Add files via upload

---
+++ else.py
@@ -0,0 +1,14 @@
+age = 31
+drunk_level = 2
+
+< is less than
+> is greater than
+<= is less than or equal to
+>= is greater than or equal to
+
+if age > 18 and drunk_level < 4:
+    print("What can I get you mate?")
+elif age == 18 and drunk_level < 3:
+    print("Are you sure want another one?")
+else:
+    print("I can't serve you, ill lose my job mate!")
